756f70514518a8c50bfa9499577a5beec10306d4
unknown <noble3769@gmail.com>
2024-03-30 11:34:55 +0900

    Day 2

---
+++ Day 2_20240330/sample.py
@@ -0,0 +1,18 @@
+print(273)
+print(52.273)
+
+# 정수 : int
+print(type(52))
+
+# 실수 : float
+print(type(52.273))
+
+# int
+print(type(0))
+
+# float
+print(type(0.0))
+
+# 참고 : 파이썬에서 지수 표현
+print(0.5228e2) # 52.28
+print(0.5228e-2) # 0.005228
