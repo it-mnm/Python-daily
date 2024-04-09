a9955276734e6f28ecb4df78f5ca1808851c2bc6
unknown <noble3769@gmail.com>
2024-04-09 17:26:09 +0900

    Day 12

---
+++ Day 12_20240409/06_return_only.py
@@ -0,0 +1,10 @@
+# 자료 없이 리턴하기
+
+# 함수를 정의합니다.
+def return_test():
+    print("A 위치입니다.")
+    return  # 함수가 종료됨
+    print("B 위치입니다.")
+
+# 함수를 호출합니다.
+return_test()
---
+++ Day 12_20240409/07_return_with_data.py
@@ -0,0 +1,7 @@
+# 자료와 함께 리턴하기
+
+# 함수를 정의합니다.
+def return_test():
+    return 100
+
+print(return_test())
